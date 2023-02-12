85b50501d778d397fb02338b09e56f3f29a3603c
Zakhar <zzz.krav@gmail.com>
2023-02-12 22:26:30 +0200

    add model Message

--- config/settings.py
+++ config/settings.py
@@ -137,11 +137,11 @@ REST_FRAMEWORK = {
 }
 
 # smtp
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'zzz.krav@gmail.com'
-EMAIL_HOST_PASSWORD = '789632145'
-EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'zzz.krav@gmail.com'
+# EMAIL_HOST_PASSWORD = '789632145'
+# EMAIL_PORT = 587
 
 
 DJOSER = {
